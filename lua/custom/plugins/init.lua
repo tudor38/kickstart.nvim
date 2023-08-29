f4137d88f25bce46331bd1110687019fd76e5263
Tudor F. Capusan <tudor@chucap.com>
2023-08-28 21:05:33 -0400

    add run current file mapping

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,7 +1,16 @@
+-- -- run file as a script depending on file type
+vim.cmd [[
+  augroup filetype_keymap
+    autocmd!
+    autocmd FileType python nmap <leader>x :w<CR>:exec "!python3" shellescape(expand(@%, 1))<CR>
+    autocmd FileType javascript nmap <leader>x :w<CR>:exec "!node" shellescape(expand(@%, 1))<CR>
+  augroup END
+]]
+
 return {
 	'tpope/vim-repeat',
 	'tpope/vim-surround',
 	'tpope/vim-obsession',
 	-- 'ggandor/leap.nvim',
-	-- 'simrat39/symbols-outline.nvim'
+	-- 'simrat39/symbols-outline.nvim',
 }
