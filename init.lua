074c9eafaf0ec17b35c9c76372f6e58cc9647d0b
Tudor F. Capusan <tudor@chucap.com>
2023-09-04 16:49:29 -0400

    clean up comments

--- init.lua
+++ init.lua
@@ -564,30 +564,3 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 
 vim.keymap.set('n', '<leader>cp', ':let @"=expand("%:p")<CR>',
   { noremap = true, silent = true, desc = 'copy current buffer absolute path' })
--- nvim-telescope/telescope-dap.nvim
--- require('telescope').load_extension('dap')
--- vim.keymap.set('n', '<leader>ds', ':Telescope dap frames<CR>', { noremap = true, silent = true, desc = 'Debug Frames' })
--- -- vim.keymap.set('n', '<leader>dc', ':Telescope dap commands<CR>')
--- vim.keymap.set('n', '<leader>db', ':Telescope dap list_breakpoints<CR>', { noremap = true, silent = true, desc = 'Debug List Breakpoints' })
---
--- -- dap
--- vim.fn.sign_define('DapBreakpoint', {text = '🟥', texthl = '', linehl = '', numhl = ''})
--- vim.fn.sign_define('DapBreakpointRejected', {text = '🟦', texthl = '', linehl = '', numhl = ''})
--- vim.fn.sign_define('DapStopped', {text = '⭐️', texthl = '', linehl = '', numhl = ''})
---
--- vim.keymap.set('n', '<leader>dh', function() require"dap".toggle_breakpoint() end, { noremap = true, silent = true, desc = 'Debug Toggle Breakpoint' })
--- vim.keymap.set('n', '<leader>dH', ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", { noremap = true, silent = true, desc = 'Debug Set Breakpoint With Condition' })
--- vim.keymap.set({'n', 't'}, '<A-k>', function() require"dap".step_out() end, { noremap = true, silent = true, desc = 'Debug Step Out' })
--- vim.keymap.set({'n', 't'}, "<A-l>", function() require"dap".step_into() end, { noremap = true, silent = true, desc = 'Debug Step Into' })
--- vim.keymap.set({'n', 't'}, '<A-j>', function() require"dap".step_over() end, { noremap = true, silent = true, desc = 'Debug Step Over' })
--- vim.keymap.set({'n', 't'}, '<A-h>', function() require"dap".continue() end, { noremap = true, silent = true, desc = 'Debug Continue' })
--- vim.keymap.set('n', '<leader>dn', function() require"dap".run_to_cursor() end, { noremap = true, silent = true, desc = 'Debug Run To Cursor' })
--- vim.keymap.set('n', '<leader>dc', function() require"dap".terminate() end, { noremap = true, silent = true, desc = 'Debug Terminate' })
--- vim.keymap.set('n', '<leader>dR', function() require"dap".clear_breakpoints() end, { noremap = true, silent = true, desc = 'Debug Clear Breakpoints' })
--- vim.keymap.set('n', '<leader>de', function() require"dap".set_exception_breakpoints({"all"}) end, { noremap = true, silent = true, desc = 'Debug Set Exception Breakpoints' })
--- vim.keymap .set('n', '<leader>di', function() require"dap.ui.widgets".hover() end, { noremap = true, silent = true, desc = 'Debug Hover' })
--- vim.keymap.set('n', '<leader>d?', function() local widgets = require "dap.ui.widgets"; widgets.centered_float(widgets.scopes) end, { noremap = true, silent = true, desc = 'Debug Float' })
--- vim.keymap.set('n', '<leader>dk', ':lua require"dap".up()<CR>zz')
--- vim.keymap.set('n', '<leader>dj', ':lua require"dap".down()<CR>zz')
--- vim.keymap.set('n', '<leader>dr', ':lua require"dap".repl.toggle({}, "vsplit")<CR><C-w>l')
--- vim.keymap.set('n', '<leader>du', ':lua require"dapui".toggle()<CR>')
