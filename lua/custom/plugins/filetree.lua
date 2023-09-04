32a2ea3d6639dfd49abc654ec9eeadc676bbbf8a
Tudor F. Capusan <tudor@chucap.com>
2023-09-04 13:47:10 -0400

    modify plugin settings neotree

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -2,10 +2,16 @@ vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
 return {
 	"nvim-neo-tree/neo-tree.nvim",
-	version = "*",
+	branch = "v3.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		{
+			"nvim-tree/nvim-web-devicons",
+			lazy = true,
+			config = function()
+				require('nvim-web-devicons').setup {}
+			end
+		}, -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
