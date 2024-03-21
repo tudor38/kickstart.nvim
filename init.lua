060c4838f22b7f84c8f3d0516b562b95f2ea10c1
Tudor Capusan <tudor@chucap.com>
2024-03-21 08:15:24 -0400

    add neotest and gen

--- init.lua
+++ init.lua
@@ -155,14 +155,14 @@ require('lazy').setup({
     },
   },
 
-  {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    main = "ibl",
-    opts = {},
-  },
+--   {
+--     -- Add indentation guides even on blank lines
+--     'lukas-reineke/indent-blankline.nvim',
+--     -- Enable `lukas-reineke/indent-blankline.nvim`
+--     -- See `:help indent_blankline.txt`
+--     main = "ibl",
+--     opts = {},
+--   },
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
--- lua/custom/plugins/gen.lua
+++ lua/custom/plugins/gen.lua
@@ -2,6 +2,6 @@
 return {
 	'David-Kunz/gen.nvim',
 	config = function()
-		require("gen").model = 'mistral:instruct'
+		require("gen").model = 'orca2:13b'
 	end,
 }
