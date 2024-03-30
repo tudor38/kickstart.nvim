8276103c8b740a9690f724e19700eb0fed319f69
Tudor Capusan <tudor@chucap.com>
2024-03-29 21:19:23 -0400

    replace default theme

--- lua/custom/plugins/theme-tokyonight.lua
+++ lua/custom/plugins/theme-tokyonight.lua
@@ -4,9 +4,9 @@ return {
 		"folke/tokyonight.nvim",
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1, -- make sure to load this before all the other start plugins
-		config = function()
-			-- load the colorscheme here
-			vim.cmd([[colorscheme tokyonight]])
-		end,
+		-- config = function()
+		-- 	-- load the colorscheme here
+		-- 	vim.cmd([[colorscheme tokyonight]])
+		-- end,
 	}
 }
