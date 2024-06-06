c8f7289dd9a58c7de0469afff1714de3b7cbde80
Tudor Capusan <tudor@chucap.com>
2024-06-06 16:18:17 -0400

    add kitty scrollback plugin

---
+++ lua/custom/plugins/kitty-scrollback.lua
@@ -0,0 +1,22 @@
+return {
+  'mikesmithgh/kitty-scrollback.nvim',
+  enabled = true,
+  lazy = true,
+  cmd = { 'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth' },
+  event = { 'User KittyScrollbackLaunch' },
+  -- version = '*', -- latest stable version, may have breaking changes if major version changed
+  version = '^4.0.0', -- pin major version, include fixes and features that do not have breaking changes
+  config = function()
+    require('kitty-scrollback').setup(
+      {
+        search = {
+          callbacks = {
+            after_ready = function()
+              vim.api.nvim_feedkeys('?', 'n', false)
+            end,
+          },
+        },
+      }
+    )
+  end,
+}
