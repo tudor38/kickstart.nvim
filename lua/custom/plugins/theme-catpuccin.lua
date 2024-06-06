2ebcca9dee03e331c586bb4f673840c8265f271b
Tudor Capusan <tudor@chucap.com>
2024-06-06 08:25:36 -0400

    simplify setting

--- lua/custom/plugins/theme-catpuccin.lua
+++ lua/custom/plugins/theme-catpuccin.lua
@@ -1,11 +1,1 @@
-return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-
-	lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	priority = 1, -- make sure to load this before all the other start plugins
-	config = function()
-		-- load the colorscheme here
-		vim.cmd([[colorscheme catppuccin]])
-	end,
-}
+return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
