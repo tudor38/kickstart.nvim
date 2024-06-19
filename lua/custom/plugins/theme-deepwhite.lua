30072a749d4f8c6af4b8f302888b3cb885a31836
Tudor Capusan <tudor@chucap.com>
2024-06-19 08:28:01 -0400

    add themes and change default theme

---
+++ lua/custom/plugins/theme-deepwhite.lua
@@ -0,0 +1,13 @@
+return {
+  'Verf/deepwhite.nvim',
+  lazy = false,
+  priority = 1000,
+  -- config = function()
+  --   require('lualine').setup({
+  --     options = {
+  --       theme = 'deepwhite',
+  --     },
+  --   })
+  --   vim.cmd([[colorscheme deepwhite]])
+  -- end,
+}
--- lua/custom/plugins/theme-molokai.lua
+++ lua/custom/plugins/theme-molokai.lua
@@ -1,12 +1,12 @@
 return {
   -- the colorscheme should be available when starting Neovim
   {
-    'navarasu/onedark.nvim',
+    'loctvl842/monokai-pro.nvim',
     lazy = false,    -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
-    -- config = function()
-    --   -- load the colorscheme here
-    --   vim.cmd([[colorscheme onedark]])
-    -- end,
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme monokai-pro]])
+    end,
   }
 }
