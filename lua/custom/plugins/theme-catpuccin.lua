ae8120c1b25d46c9454150520e2e3de4bf6f8112
Tudor Capusan <tudor@chucap.com>
2024-06-19 07:50:22 -0400

    add theme and change default theme

--- lua/custom/plugins/theme-catpuccin.lua
+++ lua/custom/plugins/theme-catpuccin.lua
@@ -4,9 +4,9 @@ return {
     name = "catppuccin",
     priority = 1000,
 
-    config = function()
-      -- load the colorscheme here
-      vim.cmd.colorscheme "catppuccin-frappe"
-    end,
+    -- config = function()
+    --   -- load the colorscheme here
+    --   vim.cmd.colorscheme "catppuccin-mocha"
+    -- end,
   }
 }
