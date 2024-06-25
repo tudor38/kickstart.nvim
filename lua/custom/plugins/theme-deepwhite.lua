5524576bc7e4a915c58dd05519c19e2b6fc89314
Tudor Capusan <tudor@chucap.com>
2024-06-25 07:11:22 -0400

    add config to colorscheme

--- lua/custom/plugins/theme-deepwhite.lua
+++ lua/custom/plugins/theme-deepwhite.lua
@@ -2,12 +2,12 @@ return {
   'Verf/deepwhite.nvim',
   lazy = false,
   priority = 1000,
-  -- config = function()
-  --   require('lualine').setup({
-  --     options = {
-  --       theme = 'deepwhite',
-  --     },
-  --   })
-  --   vim.cmd([[colorscheme deepwhite]])
-  -- end,
+  config = function()
+    require('lualine').setup({
+      options = {
+        theme = 'deepwhite',
+      },
+    })
+    --   vim.cmd([[colorscheme deepwhite]])
+  end,
 }
